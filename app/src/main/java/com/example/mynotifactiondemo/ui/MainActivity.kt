0f231c80584aa64ec4888a03a3a2d5c598245865
Valeriy Gusev <gusev.vo111@gmail.com>
2020-07-28 02:44:45 +0300

    add logout feature

--- app/src/main/java/com/example/mynotifactiondemo/ui/MainActivity.kt
+++ app/src/main/java/com/example/mynotifactiondemo/ui/MainActivity.kt
@@ -3,21 +3,53 @@ package com.example.mynotifactiondemo.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.mynotifactiondemo.R
+import com.example.mynotifactiondemo.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val loginViewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupHamburgerMenuItem()
         setupActionBar()
+        setupNavigationView()
+    }
+
+    private fun setupNavigationView() {
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            drawer_layout.closeDrawers()
+
+            when (menuItem.itemId) {
+                R.id.nav_cargo -> {
+                    Toast.makeText(this, "Cargo", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_limit -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.limitsListFragment)
+                }
+                R.id.nav_logout -> {
+                    loginViewModel.logout()
+                    findNavController(R.id.nav_host_fragment).popBackStack(R.id.launchFragment, false)
+                }
+            }
+            true
+        }
     }
 
     private fun setupHamburgerMenuItem() {
