c81f2063992e1750732ed5aeea50b237a163b41e
Valeriy Gusev <gusev.vo111@gmail.com>
2020-07-28 02:18:53 +0300

    add navigation drawer

--- app/src/main/java/com/example/mynotifactiondemo/ui/MainActivity.kt
+++ app/src/main/java/com/example/mynotifactiondemo/ui/MainActivity.kt
@@ -2,13 +2,64 @@ package com.example.mynotifactiondemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.mynotifactiondemo.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupHamburgerMenuItem()
+        setupActionBar()
+    }
+
+    private fun setupHamburgerMenuItem() {
+        val drawerToggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            R.string.action_bar_drawer_open,
+            R.string.action_bar_drawer_close
+        )
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+    }
+
+    private fun setupActionBar() {
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            android.R.id.home -> this.onHamburgerMenuItemClick()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun onHamburgerMenuItemClick(): Boolean {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+        return true
     }
 }
