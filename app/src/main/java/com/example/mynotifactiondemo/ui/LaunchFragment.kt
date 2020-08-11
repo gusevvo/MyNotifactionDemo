84c5d26d8aad13ed850b169221b073f10d29fe02
Valeriy Gusev <gusev.vo111@gmail.com>
2020-08-11 14:12:11 +0300

    add toast message on login failure

--- app/src/main/java/com/example/mynotifactiondemo/ui/LaunchFragment.kt
+++ app/src/main/java/com/example/mynotifactiondemo/ui/LaunchFragment.kt
@@ -1,13 +1,19 @@
 package com.example.mynotifactiondemo.ui
 
+import android.R.attr.animation
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.mynotifactiondemo.R
 import kotlinx.android.synthetic.main.fragment_launch.*
+
 
 class LaunchFragment : Fragment() {
 
@@ -23,7 +29,12 @@ class LaunchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         fragment_launch_button_next.setOnClickListener {  view: View ->
-            view.findNavController().navigate(R.id.action_launchFragment_to_loginFragment)
+//            view.findNavController().navigate(R.id.action_launchFragment_to_loginFragment)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                (imageView.drawable as Animatable).start()
+            }
         }
+
+
     }
 }
